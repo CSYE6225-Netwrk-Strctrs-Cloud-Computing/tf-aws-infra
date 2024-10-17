7fe2f6f24864d2cd952caa90ca11faab27e8097c
Tanuj Kodali <tanujkodali@Tanujs-MacBook-Pro-2.local>
2024-10-17 04:11:25 -0400

    pushing sg and ec2

--- variables.tf
+++ variables.tf
@@ -22,3 +22,25 @@ variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
 }
+
+variable "application_port" {
+  description = "The port on which the application runs."
+  type        = number
+  default     = 8080 // Update the default value as necessary
+}
+
+variable "key_pair_name" {
+  description = "Name of the key pair to use for EC2 instance SSH access."
+  type        = string
+}
+
+variable "custom_ami" {
+  description = "The AMI ID for the EC2 instance."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance."
+  type        = string
+  default     = "t2.micro" // Default instance type
+}
