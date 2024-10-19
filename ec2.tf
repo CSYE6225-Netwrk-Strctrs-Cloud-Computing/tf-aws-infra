d5aa62c5f55336d1bfd5fa3e3792709834f13837
Tanuj Kodali <tanujkodali@Tanujs-MacBook-Pro-2.local>
2024-10-19 18:23:23 -0400

    adding rds

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,16 @@
 resource "aws_instance" "web_app_instance" {
-  ami                    = var.custom_ami  // Use your custom AMI
-  instance_type         = var.instance_type // Reference the instance_type variable
-  vpc_security_group_ids = [aws_security_group.application_sg.id] // Attach the application security group
-  subnet_id             = element(aws_subnet.aws_tanuj_public_subnets[*].id, 0) // Assign to the first public subnet
-  associate_public_ip_address = true // Assign a public IP address
+  ami                         = var.custom_ami                                    
+  instance_type               = var.instance_type                                     
+  vpc_security_group_ids      = [aws_security_group.application_sg.id]                
+  subnet_id                   = aws_subnet.aws_tanuj_public_subnets[0].id  
+  associate_public_ip_address = true                                                
 
-  key_name = var.key_pair_name // Reference to the key pair for SSH access
+  key_name = var.key_pair_name 
 
-  // Root block device configuration
   root_block_device {
-    volume_size           = 30 // Ensure this meets the minimum required size for your AMI
-    volume_type           = "gp2" // General Purpose SSD (GP2)
-    delete_on_termination  = true // Ensure the EBS volume is terminated with the instance
+    volume_size           = 30    
+    volume_type           = "gp2" 
+    delete_on_termination = true  
   }
 
   tags = {
---
+++ rds.tf
@@ -0,0 +1,45 @@
+resource "aws_db_parameter_group" "rds_parameter_group" {
+  name_prefix = "rds-parameter-group"
+  family      = "mysql8.0"
+  description = "RDS DB parameter group for MySQL 8.0"
+
+  parameter {
+    name  = "max_connections"
+    value = "100"
+  }
+
+  parameter {
+    name  = "innodb_buffer_pool_size"
+    value = "268435456"
+  }
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "csye6225-subnet-group"
+  subnet_ids = aws_subnet.aws_tanuj_private_subnets[*].id  
+  tags = {
+    Name = "CSYE6225 RDS Subnet Group"
+  }
+}
+
+resource "aws_db_instance" "rds_instance" {
+  db_name                = var.DB_NAME
+  identifier             = var.DB_IDENTIFIER
+  engine                 = "mysql"
+  instance_class         = "db.t3.micro"
+  multi_az               = false
+  username               = var.DB_USERNAME
+  password               = var.DB_PASSWORD
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.db_security_group.id]
+  publicly_accessible    = false
+  parameter_group_name   = aws_db_parameter_group.rds_parameter_group.name
+  allocated_storage       = 20
+  skip_final_snapshot    = true
+  storage_encrypted      = true
+ 
+
+  tags = {
+    Name = "CSYE6225 RDS Instance"
+  }
+}
